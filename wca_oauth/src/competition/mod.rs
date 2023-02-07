2bc08a3cdc394d750b77a68a062a6539a59e6369
Daniel Anker Hermansen <daniel201001@gmail.com>
2023-02-07 12:41:02 +0100

    wip

--- wca_oauth/src/competition/mod.rs
+++ wca_oauth/src/competition/mod.rs
@@ -4,6 +4,24 @@ use serde::{Serialize, Deserialize};
 pub struct Competition {
     id: String,
     name: String,
+    registration_open: String,
+    registration_close: String,
+    announced_at: Option<String>,
+    start_date: String,
+    end_date: String,
+    competitor_limit: Option<u64>,
+    cancelled_at: Option<String>,
+    url: String,
+    website: String,
+    short_name: String,
+    city: String,
+    venue_adress: String,
+    latitude_degrees: f64,
+    longitude_degrees: f64,
+    country_iso2: String,
+    event_ids: Vec<String>,
+    delegates: Vec<serde_json::Value>,
+    organizers: Vec<serde_json::Value>,
 }
 
 impl Competition {
